6b01cdd54d610bcd98cf5df6625d225536848e79
Ebraheem <ave.brhom@gmail.com>
2021-03-22 10:57:16 +0200

    add connection to db

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const PORT = process.env.PORT || 4000;
+const connectDB = require("./src/database/connection");
 const cors = require('cors');
 const server = express();
 server.use(cors());
 server.use(express.json());
+connectDB();
 
 server.get("/",(req, res, next)=>{
     res.status(200).send("<h1>first open</h1>");
